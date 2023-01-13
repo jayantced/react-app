ed37f8d46df863d9b1a291ffa0fd8a6d39110e6b
jayantced739 <109576199+jayantced@users.noreply.github.com>
2023-01-13 18:25:41 +0530

    werwer

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   const toggleMode = ()=> {
     if (mode === 'light') {
       setMode('dark');
-      showAlert('success', 'Dark mode has been enabled');
+      showAlert('success', 'Dark has been enabled');
     }
     else{
       setMode('light');
